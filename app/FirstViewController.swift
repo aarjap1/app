5c61779737c57a5dfeb5132124321d5c415a3217
Piya,Aarjap Kumar <s528372@ch140005m.nwmissouri.edu>
2019-02-14 13:33:16 -0600

    Initial Commit

---
+++ app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  app
+//
+//  Created by Piya,Aarjap Kumar on 2/14/19.
+//  Copyright © 2019 Piya,Aarjap Kumar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ app/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  app
+//
+//  Created by Piya,Aarjap Kumar on 2/14/19.
+//  Copyright © 2019 Piya,Aarjap Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
